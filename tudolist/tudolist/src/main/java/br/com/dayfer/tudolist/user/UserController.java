0e775fb88d02dc6162bda7c2e7a511a4376672f0
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-10-10 00:43:44 -0300

    add postmapping

--- tudolist/tudolist/src/main/java/br/com/dayfer/tudolist/user/UserController.java
+++ tudolist/tudolist/src/main/java/br/com/dayfer/tudolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.dayfer.tudolist.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,13 @@ public class UserController {
      * Date (data)
      * void
      */
-    public void create() {
+    /*
+     * Body
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+
 
     }
 
