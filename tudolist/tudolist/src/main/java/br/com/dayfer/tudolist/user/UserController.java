45cd8d8bea1079cd4b2e209adbbde2841c1eacf4
Dayvyson Almeida <dayvysonalmeida@gmail.com>
2023-10-10 00:16:22 -0300

    add void

--- tudolist/tudolist/src/main/java/br/com/dayfer/tudolist/user/UserController.java
+++ tudolist/tudolist/src/main/java/br/com/dayfer/tudolist/user/UserController.java
@@ -1,12 +1,29 @@
 package br.com.dayfer.tudolist.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Modificador
  * public
  * private
  * protected
  */
-
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiros 
+     * Double (double) Números 0.0000
+     * Float (float) Números 0.000
+     * char (A C )
+     * Date (data)
+     * void
+     */
+    public void create() {
+
+    }
 
 }
